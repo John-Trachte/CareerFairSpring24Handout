f6d8b579b8cf470ab58b02e5cd16270b4f49d2cd
John Trachte <tracht03j@gmail.com>
2024-01-10 16:40:38 -0600

    Update pinout to reflect PCB changes

--- Code/include/pinout.h
+++ Code/include/pinout.h
@@ -3,15 +3,6 @@
 
 #define GPIO_SFR_(addr)		(*(volatile uint8_t*)(0x5000 + (addr)))
 
-#define PB_ODR			GPIO_SFR_(0x05)
-#define PB_IDR                  GPIO_SFR_(0x06)
-#define PB_DDR                  GPIO_SFR_(0x07)
-#define PB_CR1                  GPIO_SFR_(0x08)
-#define PB_CR2                  GPIO_SFR_(0x09)
-
-#define PIN_G			11
-#define PIN_E			12
-
 
 #define PC_ODR                  GPIO_SFR_(0x0A)
 #define PC_IDR                  GPIO_SFR_(0x0B)
@@ -19,8 +10,7 @@
 #define PC_CR1                  GPIO_SFR_(0x0D)
 #define PC_CR2                  GPIO_SFR_(0x0E)
 
-#define PIN_F			13
-#define PIN_D			14
+#define PIN_B			7
 
 
 #define PD_ODR                  GPIO_SFR_(0x0F)
@@ -29,6 +19,9 @@
 #define PD_CR1                  GPIO_SFR_(0x12)
 #define PD_CR2                  GPIO_SFR_(0x13)
 
-#define PIN_A			18
-#define PIN_C			19
-#define PIN_B			20
+#define PIN_A			1
+#define PIN_F			2
+#define PIN_D			3
+#define PIN_E			4
+#define PIN_G			5
+#define PIN_C			6
